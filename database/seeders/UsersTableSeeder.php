2eb7570786eabffc58d27804b22c52252c7a3152
Tashi Tshewang <tshewangtashi321@gmail.com>
2021-04-04 16:08:40 +0600

    user dummy data

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+ //     admin
+            [
+                'full_name'=>'Tashi admin',
+                'username'=>'Admin',
+                'email'=>'admin@gmail.com',
+                'password'=>Hash::make('1111'),
+                'role'=>'admin',
+                'status'=>'active'
+            ],
+//      vendor
+            [
+                'full_name'=>'Tashi vendor',
+                'username'=>'vendor',
+                'email'=>'vendor@gmail.com',
+                'password'=>Hash::make('1111'),
+                'role'=>'vendor',
+                'status'=>'active'
+            ],
+//      customer
+            [
+                'full_name'=>'Tashi customer',
+                'username'=>'Admin',
+                'email'=>'customer@gmail.com',
+                'password'=>Hash::make('1111'),
+                'role'=>'customer',
+                'status'=>'active'
+            ],
+                    ]);
+                }
+}
